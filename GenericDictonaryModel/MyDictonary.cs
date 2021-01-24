b89eb6be40fd602422f3fdb567a2ab1ee6eb9a22
USER <USER@DESKTOP-6A30JS1>
2021-01-24 12:30:05 +0300

    Add project files.

---
+++ GenericDictonaryModel/MyDictonary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericDictonaryModel
+{
+    class MyDictonary<TKey,TValue>
+    {
+        TKey[] tkey;
+        TValue[] tvalue;
+
+        public MyDictonary()
+        {
+            tkey = new TKey[0];
+            tvalue = new TValue[0];
+        }
+
+        public void Add(TKey itemKey,TValue itemValue)
+        {
+            TKey[] tempArrKey = tkey;
+            TValue[] tempArrValue = tvalue;
+            tkey = new TKey[tkey.Length + 1];
+            tvalue = new TValue[tvalue.Length + 1];
+            for (int i = 0; i < tempArrKey.Length; i++)
+            {
+                tkey[i] = tempArrKey[i];
+                tvalue[i] = tempArrValue[i];
+            }
+
+            tkey[tkey.Length - 1] = itemKey;
+            tvalue[tkey.Length - 1] = itemValue;
+
+        }
+
+        public void printAll()
+        {
+            for (int i = 0; i < tkey.Length; i++)
+            {
+                Console.WriteLine(tkey[i] + " Key , Value : " +tvalue[i]);    
+            }
+        }
+
+
+
+
+    }
+}
---
+++ GenericDictonaryModel/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericDictonaryModel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, string> myDict = new MyDictonary<int, string>();
+            myDict.Add(1, "Yazılım");
+            myDict.Add(2, "Kodlama");
+
+            myDict.printAll();
+            Console.WriteLine("******Printed******");
+            myDict.Add(4, "Eğitimi");
+            myDict.Add(5, "Teşekkürler <3");
+            myDict.printAll();
+
+
+
+
+        }
+    }
+}
